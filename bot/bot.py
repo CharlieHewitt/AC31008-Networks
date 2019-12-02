0bef45a9a4930b22a9114a4a646cb50a2891b68b
Alfie Hippisley <alfiehippisley10@gmail.com>
2019-12-02 15:06:58 +0000

    Comments Added

--- bot/bot.py
+++ bot/bot.py
@@ -8,27 +8,30 @@ Created on Wed Nov 13 09:51:22 2019
 from datetime import date
 import socket, string, calendar, datetime, random
 
-# vars specificying server
+# Variables specificying server details
 SERVER = "localhost"
 PORT = 6667
 CHANNEL = "#test"
 
 
-# open socket
+# Open the socket
 IRCSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Connect to the server
 def connect():
     IRCSocket.connect((SERVER, PORT))
     
-# hardcoded to join test
+# Hardcoded to join the channel #test
 def join():
     IRCSocket.send("JOIN #test\n".encode())
 
+# Listen to the server
 def listen():
     while (True):
         buffer = IRCSocket.recv(1024)
         message = buffer.decode()
 
+        # Check for user commands
         if ("!day" in message):
             today = date.today()
             IRCSocket.send(("PRIVMSG #test :The day today is - " + calendar.day_name[today.weekday()] + "\n").encode())
@@ -38,10 +41,11 @@ def listen():
             time = now.strftime("%H:%M:%S")
             IRCSocket.send(("PRIVMSG #test :The time right now is - " + time + "\n").encode())
 
-        # Hard coded nick name currently need to fix
+        # If the bot gets a private message
         elif ("PRIVMSG Bot" in message):
 
-            tempFacts = [
+            # Array of random facts
+            arrayFacts = [
                  "German chocolate cake is named after a guy named Sam German, not the country.",
                  "Almost as many people were killed by guillotine in Nazi Germany as in the French Revolution.",
                  "The creature that kills the most people every year isn't snakes, sharks, or even other humans — it's the mosquito.",
@@ -49,19 +53,24 @@ def listen():
                  "There's an island in Japan you can visit that's inhabited only by friendly bunnies."             
                  ]
 
+            # Identify the Nickname segment of the message
             start = ':'
             end = '!'
-            message = message[message.find(start)+len(start):message.rfind(end)]
-            #print(("\n" + "I am sending = PRIVMSG "+ message +" : Did you know - "+ tempFacts[random.randint(0,4)] + "\n"))
-            IRCSocket.send(("PRIVMSG "+ message +" :Did you know - "+ tempFacts[random.randint(0,4)] + "\n").encode())
 
+            # Get Nickname
+            message = message[message.find(start)+len(start):message.rfind(end)]
+
+            # Send the message with a random fact to the user
+            IRCSocket.send(("PRIVMSG "+ message +" :Did you know - "+ arrayFacts[random.randint(0,4)] + "\n").encode())
+
+        # Print to console
         print (buffer)
         
-#send login data (customizable)
+#Send login data (customizable)
 def login():
     IRCSocket.send("USER bot networksbot server bot\n".encode())
     IRCSocket.send("NICK Bot\n".encode())
-  #  send_data("NICK " + nickname)
+    #send_data("NICK " + nickname)
   
 connect()
 login()
