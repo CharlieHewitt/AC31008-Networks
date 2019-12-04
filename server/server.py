52f5ef64d722a4eb71b3f330ecf9e25753b26522
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2019-12-04 01:53:42 +0000

    server internal  infrastructure working

--- server/server.py
+++ server/server.py
@@ -11,24 +11,199 @@ import string
 import threading
 import time
 
-# connection class
-from Connection import Connection
+# - - - - - - - -
+# Server Details
+# - - - - - - - -
 
-# globals
 HOST = 'localhost'
-PORT = 5000
-connections = []
+PORT = 6667
+
+# - - - - - - - - - - - - - - - - -
+# User and Channel Management
+# - - - - - - - - - - - - - - - - -
+
+# dictionaries for efficient searching
+
+users = {}
+channels = {}
+connections = {}
+
+
+# Do proper exception handling / name limitation ...
+
+def addUser(userName):
+    if userName in users:
+        print('Duplicate name exception, DO ERROR HANDLING -------------')
+        return
+
+    # add user with empty channel list
+    users[userName] = []
+
+
+# Active channel connections are cleaned up on removeUser()
+
+def removeUser(userName):
+    if userName in users:
+        if users[userName]:
+            for channel in users[userName]:
+                disconnectFromChannel(userName, channel)
+        del users[userName]
+
+
+def connectToChannel(userName, channelName):
+    if userName in users and channelName in channels:
+        # add channel to users connected list
+        users[userName].append(channelName)
+
+        # add user to channel's list of connected users
+        channels[channelName].append(userName)
+
+    # return success/fail??
+
+
+def disconnectFromChannel(userName, channelName):
+    if userName in users and channelName in channels:
+        # remove user from channel
+        channels[channelName].remove(userName)
+
+        # remove channel from user's list of channels
+        users[userName].remove(channelName)
+
+
+# Send message to all users in a channel ( Add prefix/ who sent ...)
+
+
+def sendMessage(channelName, message):
+    for userName in channels[channelName]:
+        connections[userName].sendMessage(message)
+# - - - - - - - - - - -
+# Initial Server State
+# - - - - - - - - - - -
+
+# Initialise default channels here, maybe add from file later if useful but not needed
+
+
+def initialiseDefaultChannels():
+    channels['#test'] = []
+    channels['#test3'] = []
+
+
+# Dummy users for testing purposes
+
+
+def testInitialisation():
+    initialiseDefaultChannels()
+    addUser('Rachel<3')
+    addUser('Rachel<34')
+    addUser('Tom')
+    addUser('Briaaaan')
+    addUser('Xirco')
+    connectToChannel('Xirco', '#test3')
+    connectToChannel('Briaaaan', '#test')
+    connectToChannel('Briaaaan', '#test3')
+    connectToChannel('Rachel<3', '#test')
+    connectToChannel('Rachel<34', '#test')
+    connectToChannel('Tom', '#test')
+    connectToChannel('Tom', '#test34')
+    disconnectFromChannel('Tom', '#test34')
+    removeUser('Rachel<34')
+    print('Server initialised in default state:')
+    printChannels()
+    printUsers()
+
+
+# - - - - - - - -
+# Server Status
+# - - - - - - - -
+
+def serverStatus(frequency):
+    while True:
+        time.sleep(frequency)
+        print('\nCurrent Server Status: \n')
+        printChannels()
+        printUsers()
+        try:
+            connections['TinyTempah'].sendMessage('-- Status update live')
+        except:
+            print('\n-- update ping failed : TinyTempah not active')
+
+
+def printChannels():
+    print('\nChannels:\n')
+
+    for channel in channels:
+        print(channel + ' (connected users: ' + str(channels[channel]) + ')')
+
+
+def printUsers():
+    print('\nUsers:\n')
+    for user in users:
+        print(user + ' (connected to: ' + str(users[user]) + ')')
+
+
+# - - - - - - - - -
+# Connection class
+# - - - - - - - - -
+
+# TODO: initialise userName in connection initialisation message, not in constructor / hardcoded!
+#       connections aren't 'active' in the chat server ecosystem until a user is created (which requites a valid name)
+
+class Connection:
+    # Constructor
+    def __init__(self, conn, address, userName):
+        self.conn = conn
+        self.address = address
+        self.userName = userName
+
+    # toString
+    def __str__(self):
+        return "Active connection from: " + str(self.address)
+
+    # send as bytes, returns num of bytes sent
+    def sendMessage(self, message):
+        message = str(message).encode()
+        return self.conn.send(message)
+
+    # receive message
+    def receive(self):
+        return self.conn.recv(1024)
+
+    def connectToChannel(self, channelName):
+        connectToChannel(self.userName, channelName)
+
+    def disconnectFromChannel(self, channelName):
+        disconnectFromChannel(self.userName, channelName)
+
+    def listen(self):
+        try:
+            while True:
+                time.sleep(2)
+                data = self.receive()
+                if data:
+                    print('Received data from: ', self.address,
+                          'contents: ', data.decode())
+                    self.sendMessage(data)
+                addUser(self.userName)
+                self.connectToChannel('#test')
+
+                time.sleep(20)
+                self.disconnectFromChannel('#test')
+                self.sendMessage('Hello there Obi-Wan')
+
+        except ConnectionResetError:
+            # clean up open channel/user connections
+            removeUser(self.userName)
+            print('Connection dropped by: ' + str(self.address))
+
+# - - - - - - - - - - - - - -
+# Server/Socket Related Code
+# - - - - - - - - - - - - - -
 
 
 def main():
     socket = initialiseSocket()
     listenForConnections(socket)
     print('Server shutting down ...')
-
-
-def printConnections():
-    for connection in connections:
-        print(connection)
 
 
 def initialiseSocket():
@@ -39,7 +214,7 @@ def initialiseSocket():
     # bind to given port & listen for connections
     s.bind((HOST, PORT))
 
-    print('Server live on port 5000. Awaiting connections ...')
+    print('Server live on port 6667. Awaiting connections ...')
     return s
 
 
@@ -48,29 +223,25 @@ def listenForConnections(s):
     while True:
         # time.sleep(2)
         conn, addr = s.accept()
-        initialiseConnection(conn, addr)
+        initialiseConnection(conn, addr, 'TinyTempah')
+        time.sleep(5)
+        printUsers()
 
 
-def initialiseConnection(conn, addr):
+def initialiseConnection(conn, addr, userName):
     # initialise Connection & start thread
-    connection = Connection(conn, addr)
-    connection.status
-    connections.append(connection)
+    connection = Connection(conn, addr, userName)
+    connections[userName] = connection
     threading.Thread(target=connection.listen).start()
-    # printConnections()
-    print(len(connections))
 
 
-# run program
+# - - - - - - - - -
+# Program Execution
+# - - - - - - - - -
+
+# initialise server with channels and some 'ghost' users
+testInitialisation()
+
+# server status thread (args = frequency of status updates in seconds)
+threading.Thread(target=serverStatus, args=(10,)).start()
 main()
-
-
-# multi-threading example usage:
-# def runThread(name):
-#   print('Running Thread: ', name)
-#
-# thread1 = threading.Thread(target=runThread, args=('test1',))
-# thread2 = threading.Thread(target=runThread, args=('test2',))
-
-# thread1.start()
-# thread2.start()
